d1552e6a029fe537a85a4a27b3301094360177f5
mayank115 <mayank.p@atliq.com>
2023-02-21 13:42:47 +0530

    ENH: Completed with the models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.TextField()
+    image_name = models.CharField(max_length=20)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(default="", blank=True,
+                            null=False, db_index=True, unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def get_absolute_url(self):
+        return reverse('post_details', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save()
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 from datetime import date
 
 all_posts = [
-    {
-        'slug': 'hike-in-the-mountains',
-        'image': 'mountains.jpg',
-        "author": 'CoreyMS',
-        'date': date(2022, 12, 13),
-        'title': 'Mountain Hiking',
-        'excerpt': 'There is nothing like the views you get when hiking in the mountains! And I wasn\'t\ even prepared for what happened whilst I was enjoying the view!',
-        'content': """Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae at illum itaque. Expedita odio iste quaerat non laborum et quia blanditiis repellat, adipisci ducimus, quasi possimus iure quo voluptatum vel.\n
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae at illum itaque. Expedita odio iste quaerat non laborum et quia blanditiis repellat, adipisci ducimus, quasi possimus iure quo voluptatum vel.\n
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae at illum itaque. Expedita odio iste quaerat non laborum et quia blanditiis repellat, adipisci ducimus, quasi possimus iure quo voluptatum vel."""
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio. 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio. 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis aperiam est praesentium, quos iste consequuntur omnis exercitationem quam velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
+
 ]
 
 
@@ -47,15 +14,16 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, 'blog/index.html', {"posts": latest_posts})
+    sorted_posts = Post.objects.all().order_by('-date')[:3]
+    # latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {"posts": sorted_posts})
 
 
 def posts(request):
+    all_posts = Post.objects.all()
     return render(request, 'blog/all-posts.html', {"all_posts": all_posts})
 
 
 def post_details(request, slug):
-    selected_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, 'blog/post-detail.html', {"post": selected_post})
+    selected_post = Post.objects.get(slug=slug)
+    return render(request, 'blog/post-detail.html', {"post": selected_post, "post_tags": selected_post.tags.all()})
